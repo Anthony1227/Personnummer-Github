3264718ddff5d205620135c36733bcebb68598b7
anthony tran <anth.tran00@gmail.com>
2020-10-27 00:31:03 +0100

    Amount method and Year method

--- Personnummer Github/Program.cs
+++ Personnummer Github/Program.cs
@@ -11,10 +11,19 @@ namespace Personnummer_Github
         static void Main(string[] args)
         {
             DrawMenu();
+            try
+            {
+                string input = Console.ReadLine();
 
-            string input = Console.ReadLine();
+                AmountCheck(input);
+                YearCheck(input);
 
-
+               
+            }
+            catch
+            {
+                Console.WriteLine("SSN is not correct");
+            } 
             Console.ReadKey();
         }
 
@@ -27,6 +36,33 @@ namespace Personnummer_Github
             Console.WriteLine("* Enter 12 digit Social Security Number *");
             Console.WriteLine("*                                       *");
             Console.WriteLine("*****************************************");
+        }
+
+        //Method that checks if the input is a 12 character string
+        static bool AmountCheck(string input)
+        {
+            int amount = 0;
+
+            foreach (var num in input)
+            {
+                amount += 1;
+            }
+
+            if (amount != 12)
+                return false;
+            else
+                return true;
+        }
+
+        //Method that checks if the year is between 1753 and 2020
+        static bool YearCheck(string input)
+        {
+            string year = input.Substring(0, 4);
+
+            if (Int32.Parse(year) <= 2020 && Int32.Parse(year) >= 1753)
+                return true;
+            else
+                return false;
         }
     }
 }
