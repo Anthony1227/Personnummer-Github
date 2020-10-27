3ac38b1b0d341fa8f39417e12e299d76df99db16
anthony tran <anth.tran00@gmail.com>
2020-10-27 12:06:57 +0100

    Output

--- Personnummer Github/Program.cs
+++ Personnummer Github/Program.cs
@@ -18,6 +18,11 @@ namespace Personnummer_Github
                 AmountCheck(input);
                 YearCheck(input);
                 MonthCheck(input);
+                DayCheck(input);
+                DateExist(input);
+                BirthDayNumCheck(input);
+                IsMale(input);
+                Output(input);
                
             }
             catch
@@ -77,6 +82,74 @@ namespace Personnummer_Github
             else
             {
                 return false;
+            }
+        }
+
+        //Method that checks if the day is between 1 and 31
+
+        static bool DayCheck(string input)
+        {
+            string day = input.Substring(6, 2);
+
+            if (Int32.Parse(day) <= 31 && Int32.Parse(day) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Method that checks if the date exists
+        static bool DateExist(string input)
+        {
+            string year = input.Substring(0, 4);
+            string month = input.Substring(4, 2);
+            string day = input.Substring(6, 2);
+            return Int32.Parse(day) <= DateTime.DaysInMonth(Int32.Parse(year), Int32.Parse(month));
+        }
+
+        //Method that checks if the birthday number is between 1 and 999
+        static bool BirthDayNumCheck(string input)
+        {
+            string birthDayNum = input.Substring(8, 3);
+
+            if (Int32.Parse(birthDayNum) <= 999 && Int32.Parse(birthDayNum) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Method checks if the person is a male
+        static bool IsMale(string input)
+        {
+            string genderNum = input.Substring(10, 1);
+
+            if (Int32.Parse(genderNum) % 2 == 0)
+                return false;
+            else
+                return true;
+        }
+
+        //Outputs diffrent messages based on result
+        static void Output(string input)
+        {
+            if (AmountCheck(input) && YearCheck(input) && MonthCheck(input) && DayCheck(input) && BirthDayNumCheck(input) && DateExist(input))
+            {
+                Console.WriteLine("ssn is correct");
+                if (IsMale(input))
+                    Console.WriteLine("person is Male");
+                else
+                    Console.WriteLine("person is Woman");
+            }
+            else
+            {
+                Console.WriteLine("ssn is not correct");
             }
         }
     }
